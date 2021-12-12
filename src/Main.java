97f809ae8a29583602aecff4e5b607dfcbd90569
Sergei Rtischev <sergei1502@yandex.ru>
2021-12-12 17:49:50 +0500

    Adapter v1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.sum(4, 2));
+        System.out.println(calc.mult(10, 3));
+        System.out.println(calc.pow(6, 8));
+    }
+}
